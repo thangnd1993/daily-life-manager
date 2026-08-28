0759ef0e5d0112021df50b80ba8c2260f4cac6be
Thang Nguyen <thang.nguyen@smartec.com.vn>
2026-08-29 03:23:00 +0700

    test(admin): run component tests with Jest

---
+++ apps/admin-web/jest.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  preset: 'jest-preset-angular',
+  setupFilesAfterEnv: ['<rootDir>/setup-jest.ts'],
+  testEnvironment: 'jsdom',
+  testMatch: ['<rootDir>/src/**/*.spec.ts'],
+};
